40df206b42d6c1fa0831916cfdeb38d486ea0e5d
Shrizo <shreeramsls@gmail.com>
2021-07-14 02:16:44 -0400

    Add mongodb connection via mongoose to nodejs/expressjs app

--- api/app.js
+++ api/app.js
@@ -3,15 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var db = require('./db')
 
-var indexRouter = require('./routes/index');
+require('dotenv').config();
+
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+db.connect(process.env.MONGO_URI);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
