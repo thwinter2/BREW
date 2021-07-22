d585b7ae4c219e32e112f18f78597c746211cb9c
molinamelendezj <jose.molina-melendez@allscripts.com>
2021-07-22 14:43:33 -0400

    user with back end calls and mongo db

---
+++ api/server.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./models/User.js");
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: ["somesecretsauce"]
+  })
+);
+
+// MongoDB configuration
+const db = process.env.MONGO_URI;
+
+// Use mongoose to connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch(err => console.log(err));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport config
+require("./passport");
+
+require("./routes/auth.js")(app);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`App running on port ${port}`));
--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,35 @@
-import './App.css';
-import React from 'react';
-import Login from './components/login';
-import Logout from './components/logout';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Profile from "./components/profile/Profile";
+import Landing from "./components/landing/Landing";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import Map from './components/map';
 
-function App() {
-  return (
-    <div className="App">
-      <Login />
-      <Logout />
-      <Map />
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/profile" component={Profile} />
+            </div>
+            <Map />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
+
